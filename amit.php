4237c8e46ad5748b5cfc3cf89d616ef828b84f6b
Amit Dhussa <62895271+Amit-Dhussa@users.noreply.github.com>
2021-06-16 14:20:48 +0530

    Add files via upload

---
+++ amit.php
@@ -0,0 +1,178 @@
+<?php require_once("includes/DB.php"); ?>
+<?php require_once("includes/functions.php"); ?>
+<?php require_once("includes/sessions.php"); ?>
+<?php $SearchQueryParameter = $_GET["id"]; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+  <link rel="stylesheet" href="Css/Style.css">
+  
+  <title>Full Post Page</title>
+
+</head>
+<body>
+  <!-- NAVBAR -->
+  <div style="height:10px; background:#27aae1;"></div>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <a href="#" class="navbar-brand"> JAZEBAKRAM.COM</a>
+      <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarcollapseCMS">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarcollapseCMS">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a href="Blog.php?page=1" class="nav-link">Home</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">About Us</a>
+        </li>
+        <li class="nav-item">
+          <a href="Blog.php?page=1" class="nav-link">Blog</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">Contact Us</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">Features</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav ml-auto">
+        <form class="form-inline d-none d-sm-block" action="Blog.php">
+          <div class="form-group">
+          <input class="form-control mr-2" type="text" name="Search" placeholder="Search here"value="">
+          <button  class="btn btn-primary" name="SearchButton">Go</button>
+          </div>
+        </form>
+      </ul>
+      </div>
+    </div>
+  </nav>
+    <div style="height:10px; background:#27aae1;"></div>
+    <!-- NAVBAR END -->
+    <!-- HEADER -->
+    <div class="container">
+      <div class="row mt-4">
+        <!-- Main Area Start-->
+        <div class="col-sm-8 ">
+          <h1>The Complete Responsive CMS Blog</h1>
+          <h1 class="lead">The Complete blog by using PHP by Jazeb Akram</h1>
+          <?php
+           echo ErrorMessage();
+           echo SuccessMessage();
+           ?>
+          <?php
+          global $ConnectingDB;
+          // SQL query when Searh button is active
+          if(isset($_GET["SearchButton"])){
+            $Search = $_GET["Search"];
+            $sql = "SELECT * FROM posts
+            WHERE datetime LIKE :search
+            OR title LIKE :search
+            OR category LIKE :search
+            OR post LIKE :search";
+            $stmt = $ConnectingDB->prepare($sql);
+            $stmt->bindValue(':search','%'.$Search.'%');
+            $stmt->execute();
+          }
+          // The default SQL query
+          else{
+            $PostIdFromURL = $_GET["id"];
+            if (!isset($PostIdFromURL)) {
+              $_SESSION["ErrorMessage"]="Bad Request !";
+              Redirect_to("Blog.php?page=1");
+            }
+            $sql  = "SELECT * FROM posts  WHERE id= '$PostIdFromURL'";
+            $stmt =$ConnectingDB->query($sql);
+           
+
+          }
+          while ($DataRows = $stmt->fetch()) {
+            $PostId          = $DataRows["id"];
+            $DateTime        = $DataRows["datetime"];
+            $PostTitle       = $DataRows["title"];
+            $Category        = $DataRows["category"];
+            $Admin           = $DataRows["author"];
+            $Image           = $DataRows["image"];
+            $PostDescription = $DataRows["post"];
+          ?>
+          <div class="card">
+            <img src="upload/<?php echo htmlentities($Image); ?>" style="max-height:450px;" class="img-fluid card-img-top" />
+            <div class="card-body">
+              <h4 class="card-title"><?php echo htmlentities($PostTitle); ?></h4>
+              <small class="text-muted">Category: <span class="text-dark"> <a href="Blog.php?category=<?php echo htmlentities($Category); ?>"> <?php echo htmlentities($Category); ?> </a></span> & Written by <span class="text-dark"> <a href="Profile.php?username=<?php echo htmlentities($Admin); ?>"> <?php echo htmlentities($Admin); ?></a></span> On <span class="text-dark"><?php echo htmlentities($DateTime); ?></span></small>
+                  <!-- For Comment -->
+         <span style="float:right;" class="badge badge-dark text-light  ">Comments 20</span>
+
+         <!-- For Like -->
+          <span style="float:right;" class="badge badge-primary text-light mr-1"><i class="far fa-thumbs-up"></i></span>
+
+          <!-- For Fire -->
+           <span style="float:right;" class="badge badge-danger text-light mr-1"><i class="fas fa-fire-alt"></i></i></span>
+          <hr />
+         <p class="card-text">
+           <?php
+             echo htmlentities($PostDescription);
+           ?>
+        </p>
+         <!-- <a href="FullPost.php?id=<?php echo $PostId;?>" style="float:right;"><span class="btn btn-info">Read More >> </span>
+         </a> -->
+       </div>
+     </div>
+   <?php } ?>
+
+    </div>  
+
+  
+
+
+        <!-- Main Area End-->
+
+        <!-- Side Area Start -->
+    <div class="col-sm-4" style="min-height:40px; background:green;">
+
+    </div>
+    <!-- Side Area End -->
+  </div>
+</div>
+<!---End Header--->
+<br />
+<!---Footer--->
+<footer class="bg-dark text-white" >
+  <div class="container">
+  <div class="row">
+    <div class="col">
+     <p class="lead text-center">
+       Theme By | Amit Dhussa| <span id="year"></span>&copy;----All right Reserved.
+     </p>
+     <p class="text-center small"><a style="color: white; text-decoration: none; cursor: pointer;" href="http://jazebakram.com/coupons/" target="_blank"> This site is only used for Study purpose amitdhussa.com have all the rights. no one is allow to distribute copies other then <br>&trade; jazebakram.com &trade;  Udemy ; &trade; Skillshare ; &trade; StackSkills</a>
+     </p>
+    </div>
+  </div>
+  </div>
+</footer>
+<div style=" height:10px; background:#27aae1;"></div>
+<!----End Footer--->
+
+<!---Bootstrap Jquery CDN link---->
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-
+Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-
+OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+<!----Javascript date function--->
+<script>
+ $('#year').text(new Date().getFullYear());
+</script>
+
+  </body>
+</html>
